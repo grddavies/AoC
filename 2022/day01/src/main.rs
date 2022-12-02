71d51ed0c7262e9e9967fc1caf1f13a017d1eca7
Gethin Davies <grd.davies@gmail.com>
2022-12-02 19:14:20 +0000

    Solve part 2

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,20 +1,28 @@
 use std::io::{self, BufRead};
 
+fn insert_in_place<T>(arr: &mut [T], value: T, idx: usize) {
+    arr[idx..].rotate_right(1);
+    arr[idx] = value;
+}
+
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
-    let mut max = 0;
+    let mut maxes = [0, 0, 0];
     let mut agg = 0;
     for line in stdin.lock().lines() {
         match line.unwrap().as_str() {
             "" => {
-                if agg > max {
-                    max = agg;
+                for i in 0..maxes.len() {
+                    if &agg > &maxes[i] {
+                        insert_in_place(&mut maxes, agg, i);
+                        break
+                    }
                 }
                 agg = 0
             }
             line => agg += line.parse::<i32>().unwrap(),
         }
     }
-    println!("{}", max);
+    println!("{}", maxes.iter().sum::<i32>());
     Ok(())
 }
