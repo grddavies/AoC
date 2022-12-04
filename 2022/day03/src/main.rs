85a5b9151556e432a8d708d30f68251339f54ee5
Gethin Davies <grd.davies@gmail.com>
2022-12-04 05:07:34 +0000

    Solve part 2

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -1,27 +1,38 @@
-use std::io::{self, BufRead};
+use std::{
+    io::{self, BufRead},
+    vec::Vec,
+};
 
 fn get_priority(c: char) -> i32 {
-    match c.is_uppercase() {
-        true => c as i32 - 65 + 27,
-        false => c as i32 - 97 + 1,
+    match c {
+        'a'..='z' => (c as u8 - 'a' as u8 + 1).into(),
+        'A'..='Z' => (c as u8 - 'A' as u8 + 27).into(),
+        _ => 0
     }
 }
 
 fn main() {
     let stdin = io::stdin();
+
+    let lines = stdin
+        .lock()
+        .lines()
+        .filter_map(|x| x.ok())
+        .collect::<Vec<String>>();
+
     let mut total = 0;
-    for res in stdin.lock().lines() {
-        if let Ok(line) = res {
-            let (left, right) = line.split_at(line.len() / 2);
-            let mut subtotal = 0;
-            for lc in left.chars() {
-                if right.chars().any(|rc| rc == lc) {
-                    subtotal += get_priority(lc);
-                    break; // only one char will be repeated
-                }
-            }
-            total += subtotal;
-        }
-    }
-    println!("{}", total)
+    lines.chunks(3).for_each(|thing| {
+        match thing {
+            [first, second, third] => {
+                match first
+                    .chars()
+                    .find(|c| second.contains(*c) && third.contains(*c)) {
+                        Some(common_char) => total += get_priority(common_char),
+                        None => panic!("No common characters in these groups:\n{}\n{}\n{}", first, second, third )
+                    }
+            },
+            _ => panic!("Didn't expect to get here! Are you feeding me lines in threes?")
+        }            
+    });
+    println!("{}", total);
 }
